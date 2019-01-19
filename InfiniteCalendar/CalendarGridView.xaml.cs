1c9b382585cde618dc703f19f05a13ed3ebf5bd8
Kevin <kevinhle@outlook.com>
2019-01-19 13:15:24 -0600

    make cosmetic changes

--- InfiniteCalendar/CalendarGridView.xaml.cs
+++ InfiniteCalendar/CalendarGridView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace InfiniteCalendar
 {
@@ -10,6 +11,12 @@ namespace InfiniteCalendar
         public CalendarGridView()
         {
             InitializeComponent();
+
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+        }
+        private void OnCancelClicked(object sender, EventArgs args)
+        {
+            Xamarin.Forms.Application.Current.MainPage.Navigation.PopModalAsync();
         }
     }
 }
