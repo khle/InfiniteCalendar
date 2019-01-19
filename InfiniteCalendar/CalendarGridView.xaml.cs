d6e27fbc71355ef5a08334f0502fd40927ee29c5
Kevin <kevinhle@outlook.com>
2019-01-19 12:17:13 -0600

    first commit

---
+++ InfiniteCalendar/CalendarGridView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace InfiniteCalendar
+{
+    public partial class CalendarGridView : ContentPage
+    {
+        public CalendarGridView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ InfiniteCalendar/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace InfiniteCalendar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void LaunchCalendarClicked(object sender, EventArgs e)
+        {
+            var page = new Xamarin.Forms.NavigationPage(new CalendarGridView());
+            await Application.Current.MainPage.Navigation.PushModalAsync(page, true);
+        }
+    }
+}
